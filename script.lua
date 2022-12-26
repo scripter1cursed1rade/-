083319c55e03eb51ab525f7dea8f1869e361bd6e
scripter1cursed1rade <120814654+scripter1cursed1rade@users.noreply.github.com>
2022-12-26 20:10:52 +0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,528 @@
+local guitrollv10 = Instance.new("ScreenGui") 
+local Frame = Instance.new("Frame") 
+local MAINFRAM = Instance.new("Frame") 
+local top = Instance.new("TextBox") 
+local PROJECT = Instance.new("TextLabel") 
+local GrabKnifeV3 = Instance.new("TextButton") 
+local Wing = Instance.new("TextButton") 
+local MADEBY = Instance.new("TextLabel") 
+local LGYHUB = Instance.new("TextButton") 
+local MILK = Instance.new("TextButton") 
+local Gun = Instance.new("TextButton") 
+local KohlsAdmin = Instance.new("TextButton") 
+local Shotgun = Instance.new("TextButton") 
+local BrutalAntiFurry = Instance.new("TextButton") 
+local Name = Instance.new("TextLabel") 
+local Setting = Instance.new("TextButton") 
+local Exit = Instance.new("TextButton") 
+local feantiban = Instance.new("TextLabel")
+local KICKGUI = Instance.new("TextLabel")
+local ControlGui = Instance.new("TextButton")
+local LuaHammer = Instance.new("TextButton")
+local ClownVan = Instance.new("TextButton")
+local BoogaKuy = Instance.new("TextButton")
+local Tec = Instance.new("TextButton")
+local COMINGSOON = Instance.new("TextButton")
+local guitest = Instance.new("TextLabel")
+local ComingSoon = Instance.new("TextButton")
+local powerbyguest = Instance.new("TextLabel")
+local BootyOffenderV2 = Instance.new("TextButton")
+local AnimationHUB = Instance.new("TextButton")
+local SETTINGS = Instance.new("TextButton")
+local Open = Instance.new("TextButton")
+--Properties:
+guitrollv10.Name = "guitrollv10"
+guitrollv10.Parent = game.CoreGui
+ 
+ 
+Frame.Parent = guitrollv10
+Frame.Active = true
+Frame.BackgroundColor3 = Color3.new(1, 1, 1)
+Frame.BorderColor3 = Color3.new(1, 1, 1)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.289494812, 0, 0.127559051, 0)
+Frame.Selectable = true
+Frame.Size = UDim2.new(0, 733, 0, 456)
+Frame.Style = Enum.FrameStyle.RobloxRound
+Frame.Draggable = true
+ 
+ 
+MAINFRAM.Name = "MAINFRAM"
+MAINFRAM.Parent = Frame
+MAINFRAM.Active = true
+MAINFRAM.BackgroundColor3 = Color3.new(1, 1, 1)
+MAINFRAM.BorderColor3 = Color3.new(0, 1, 1)
+MAINFRAM.Position = UDim2.new(-0.00130788609, 0, 0.0511506721, 0)
+MAINFRAM.Selectable = true
+MAINFRAM.Size = UDim2.new(0, 717, 0, 411)
+MAINFRAM.Style = Enum.FrameStyle.RobloxRound
+ 
+ 
+top.Name = "top"
+top.Parent = MAINFRAM
+top.BackgroundColor3 = Color3.new(0, 0, 0)
+top.BorderColor3 = Color3.new(0.333333, 1, 1)
+top.Position = UDim2.new(-0.00675498322, 0, 1.01742566, 0)
+top.Size = UDim2.new(0, 710, 0, 14)
+top.Font = Enum.Font.SourceSans
+top.MultiLine = true
+top.Text = ""
+top.TextColor3 = Color3.new(0, 0, 0)
+top.TextSize = 14
+top.TextWrapped = true
+ 
+ 
+PROJECT.Name = "PROJECT"
+PROJECT.Parent = Frame
+PROJECT.Active = true
+PROJECT.BackgroundColor3 = Color3.new(1, 0, 0)
+PROJECT.Position = UDim2.new(0.00272851298, 0, 0.914627612, 0)
+PROJECT.Selectable = true
+PROJECT.Size = UDim2.new(0, 200, 0, 29)
+PROJECT.Font = Enum.Font.Gotham
+PROJECT.Text = "PROJECT BY.GUESTNOOB"
+PROJECT.TextColor3 = Color3.new(0, 0, 0)
+PROJECT.TextScaled = true
+PROJECT.TextSize = 30
+PROJECT.TextWrapped = true
+ 
+ 
+GrabKnifeV3.Name = "GrabKnifeV3"
+GrabKnifeV3.Parent = Frame
+GrabKnifeV3.BackgroundColor3 = Color3.new(1, 0, 0)
+GrabKnifeV3.BorderColor3 = Color3.new(0.333333, 1, 1)
+GrabKnifeV3.Position = UDim2.new(0.175836697, 0, 0.073659122, 0)
+GrabKnifeV3.Size = UDim2.new(0, 102, 0, 30)
+GrabKnifeV3.Font = Enum.Font.Gotham
+GrabKnifeV3.Text = "GRAB KNIFE V3"
+GrabKnifeV3.TextColor3 = Color3.new(0, 0, 0)
+GrabKnifeV3.TextScaled = true
+GrabKnifeV3.TextSize = 14
+GrabKnifeV3.TextWrapped = true
+ 
+ 
+Wing.Name = "Wing"
+Wing.Parent = Frame
+Wing.BackgroundColor3 = Color3.new(1, 0, 0)
+Wing.BorderColor3 = Color3.new(0, 1, 1)
+Wing.Position = UDim2.new(0.0218510479, 0, 0.0721301138, 0)
+Wing.Size = UDim2.new(0, 104, 0, 31)
+Wing.Font = Enum.Font.Gotham
+Wing.Text = "Wing"
+Wing.TextColor3 = Color3.new(0, 0, 0)
+Wing.TextScaled = true
+Wing.TextSize = 14
+Wing.TextWrapped = true
+ 
+ 
+MADEBY.Name = "MADEBY"
+MADEBY.Parent = Frame
+MADEBY.BackgroundColor3 = Color3.new(1, 0, 0)
+MADEBY.Position = UDim2.new(0.693301797, 0, 0.912919104, 0)
+MADEBY.Size = UDim2.new(0, 215, 0, 29)
+MADEBY.Font = Enum.Font.Gotham
+MADEBY.Text = "MADE BY. PEEMAI GAMERTV"
+MADEBY.TextColor3 = Color3.new(0, 0, 0)
+MADEBY.TextScaled = true
+MADEBY.TextSize = 14
+MADEBY.TextWrapped = true
+ 
+ 
+LGYHUB.Name = "LGYHUB"
+LGYHUB.Parent = Frame
+LGYHUB.BackgroundColor3 = Color3.new(1, 0, 0)
+LGYHUB.BorderColor3 = Color3.new(0.333333, 1, 1)
+LGYHUB.Position = UDim2.new(0.328433067, 0, 0.0724560916, 0)
+LGYHUB.Size = UDim2.new(0, 106, 0, 30)
+LGYHUB.Font = Enum.Font.Gotham
+LGYHUB.Text = " LEGACY HUB "
+LGYHUB.TextColor3 = Color3.new(0, 0, 0)
+LGYHUB.TextScaled = true
+LGYHUB.TextSize = 14
+LGYHUB.TextWrapped = true
+ 
+ 
+MILK.Name = "MILK"
+MILK.Parent = Frame
+MILK.BackgroundColor3 = Color3.new(1, 0, 0)
+MILK.BorderColor3 = Color3.new(0.333333, 1, 1)
+MILK.Position = UDim2.new(0.650011897, 0, 0.0747869387, 0)
+MILK.Size = UDim2.new(0, 104, 0, 29)
+MILK.Selected = true
+MILK.Font = Enum.Font.Gotham
+MILK.Text = "Bleach Drinking 2.0"
+MILK.TextColor3 = Color3.new(0, 0, 0)
+MILK.TextScaled = true
+MILK.TextSize = 14
+MILK.TextWrapped = true
+ 
+ 
+Gun.Name = "Gun"
+Gun.Parent = Frame
+Gun.BackgroundColor3 = Color3.new(1, 0, 0)
+Gun.BorderColor3 = Color3.new(0.333333, 1, 1)
+Gun.Position = UDim2.new(0.809842348, 0, 0.0746247098, 0)
+Gun.Size = UDim2.new(0, 118, 0, 30)
+Gun.Font = Enum.Font.Gotham
+Gun.Text = "Dms Suicide Pistol"
+Gun.TextColor3 = Color3.new(0, 0, 0)
+Gun.TextScaled = true
+Gun.TextSize = 14
+Gun.TextWrapped = true
+ 
+ 
+KohlsAdmin.Name = "KohlsAdmin"
+KohlsAdmin.Parent = Frame
+KohlsAdmin.BackgroundColor3 = Color3.new(1, 0, 0)
+KohlsAdmin.BorderColor3 = Color3.new(0.333333, 1, 1)
+KohlsAdmin.Position = UDim2.new(0.0211174991, 0, 0.172669396, 0)
+KohlsAdmin.Size = UDim2.new(0, 104, 0, 35)
+KohlsAdmin.Font = Enum.Font.GothamBold
+KohlsAdmin.Text = "Kohls Admin Gui"
+KohlsAdmin.TextColor3 = Color3.new(0, 0, 0)
+KohlsAdmin.TextScaled = true
+KohlsAdmin.TextSize = 14
+KohlsAdmin.TextWrapped = true
+ 
+ 
+Shotgun.Name = "Shotgun"
+Shotgun.Parent = Frame
+Shotgun.BackgroundColor3 = Color3.new(1, 0, 0)
+Shotgun.BorderColor3 = Color3.new(0.333333, 1, 1)
+Shotgun.Position = UDim2.new(0.175306961, 0, 0.174729124, 0)
+Shotgun.Size = UDim2.new(0, 103, 0, 34)
+Shotgun.Font = Enum.Font.GothamBlack
+Shotgun.Text = " Shotgun "
+Shotgun.TextColor3 = Color3.new(0, 0, 0)
+Shotgun.TextScaled = true
+Shotgun.TextSize = 14
+Shotgun.TextWrapped = true
+ 
+ 
+BrutalAntiFurry.Name = "Brutal Anti-Furry"
+BrutalAntiFurry.Parent = Frame
+BrutalAntiFurry.BackgroundColor3 = Color3.new(1, 0, 0)
+BrutalAntiFurry.BorderColor3 = Color3.new(0.333333, 1, 1)
+BrutalAntiFurry.Position = UDim2.new(0.32657823, 0, 0.173836559, 0)
+BrutalAntiFurry.Size = UDim2.new(0, 107, 0, 35)
+BrutalAntiFurry.Font = Enum.Font.GothamSemibold
+BrutalAntiFurry.Text = "Brutal Anti-Furry"
+BrutalAntiFurry.TextColor3 = Color3.new(0, 0, 0)
+BrutalAntiFurry.TextScaled = true
+BrutalAntiFurry.TextSize = 14
+BrutalAntiFurry.TextWrapped = true
+ 
+ 
+Name.Name = "Name"
+Name.Parent = Frame
+Name.BackgroundColor3 = Color3.new(0, 1, 0)
+Name.BorderColor3 = Color3.new(0, 1, 1)
+Name.Position = UDim2.new(0.179473564, 0, -0.017177783, 0)
+Name.Size = UDim2.new(0, 528, 0, 25)
+Name.Font = Enum.Font.Gotham
+Name.Text = "            GUI TROLLING  V10.3               "
+Name.TextColor3 = Color3.new(0, 0, 0)
+Name.TextSize = 40
+ 
+ 
+Setting.Name = "Setting"
+Setting.Parent = Name
+Setting.BackgroundColor3 = Color3.new(1, 0, 0)
+Setting.BorderColor3 = Color3.new(0.333333, 1, 1)
+Setting.Position = UDim2.new(0.840909064, 0, 0, 0)
+Setting.Size = UDim2.new(0, 84, 0, 25)
+Setting.Font = Enum.Font.Gotham
+Setting.Text = "SETTING"
+Setting.TextColor3 = Color3.new(0, 0, 0)
+Setting.TextScaled = true
+Setting.TextSize = 14
+Setting.TextWrapped = true
+ 
+ 
+Exit.Name = "Exit"
+Exit.Parent = Frame
+Exit.BackgroundColor3 = Color3.new(0, 0, 0)
+Exit.BorderColor3 = Color3.new(0.333333, 1, 1)
+Exit.Position = UDim2.new(0.916072249, 0, -0.0175536461, 0)
+Exit.Size = UDim2.new(0, 68, 0, 25)
+Exit.Font = Enum.Font.Gotham
+Exit.Text = "EXIT"
+Exit.TextColor3 = Color3.new(1, 0, 0)
+Exit.TextScaled = true
+Exit.TextSize = 14
+Exit.TextWrapped = true
+Exit.MouseButton1Down:connect(function()
+Open.Visible = true
+Frame.Visible = false
+MAINFRAM.Visible = false
+end)
+ 
+ 
+feantiban.Name = "fe antiban"
+feantiban.Parent = Frame
+feantiban.Active = true
+feantiban.BackgroundColor3 = Color3.new(0, 0.666667, 1)
+feantiban.BorderColor3 = Color3.new(0.333333, 1, 1)
+feantiban.Position = UDim2.new(-0.0114875156, 0, -0.0159192346, 0)
+feantiban.Selectable = true
+feantiban.Size = UDim2.new(0, 192, 0, 25)
+feantiban.Font = Enum.Font.Gotham
+feantiban.Text = "FE-ANTIBAN : ON"
+feantiban.TextColor3 = Color3.new(0, 1, 0)
+feantiban.TextScaled = true
+feantiban.TextSize = 14
+feantiban.TextStrokeColor3 = Color3.new(0, 1, 1)
+feantiban.TextWrapped = true
+feantiban.TextYAlignment = Enum.TextYAlignment.Top
+ 
+ 
+KICKGUI.Name = "KICKGUI"
+KICKGUI.Parent = Frame
+KICKGUI.BackgroundColor3 = Color3.new(0, 0.666667, 1)
+KICKGUI.Position = UDim2.new(-0.00164428493, 0, 0.741462111, 0)
+KICKGUI.Size = UDim2.new(0, 200, 0, 29)
+KICKGUI.Font = Enum.Font.GothamBlack
+KICKGUI.Text = "BOOGA BOOGA GUI"
+KICKGUI.TextColor3 = Color3.new(0, 0, 0)
+KICKGUI.TextScaled = true
+KICKGUI.TextSize = 14
+KICKGUI.TextWrapped = true
+ 
+ 
+ControlGui.Name = "Control Gui"
+ControlGui.Parent = Frame
+ControlGui.BackgroundColor3 = Color3.new(1, 0, 0)
+ControlGui.BorderColor3 = Color3.new(0, 1, 1)
+ControlGui.Position = UDim2.new(0.488078326, 0, 0.174778581, 0)
+ControlGui.Size = UDim2.new(0, 104, 0, 34)
+ControlGui.Font = Enum.Font.SourceSansBold
+ControlGui.Text = "Contro Gui"
+ControlGui.TextColor3 = Color3.new(0, 0, 0)
+ControlGui.TextScaled = true
+ControlGui.TextSize = 14
+ControlGui.TextWrapped = true
+ 
+ 
+LuaHammer.Name = "LuaHammer"
+LuaHammer.Parent = Frame
+LuaHammer.BackgroundColor3 = Color3.new(0, 1, 1)
+LuaHammer.BorderColor3 = Color3.new(1, 1, 1)
+LuaHammer.Position = UDim2.new(0.808622718, 0, 0.175929025, 0)
+LuaHammer.Size = UDim2.new(0, 119, 0, 34)
+LuaHammer.Font = Enum.Font.GothamBlack
+LuaHammer.Text = "LuaHammer"
+LuaHammer.TextColor3 = Color3.new(0, 0, 0)
+LuaHammer.TextScaled = true
+LuaHammer.TextSize = 14
+LuaHammer.TextWrapped = true
+ 
+ 
+ClownVan.Name = "ClownVan"
+ClownVan.Parent = Frame
+ClownVan.BackgroundColor3 = Color3.new(1, 0, 0)
+ClownVan.BorderColor3 = Color3.new(0, 1, 1)
+ClownVan.Position = UDim2.new(0.651015639, 0, 0.1749098, 0)
+ClownVan.Size = UDim2.new(0, 104, 0, 34)
+ClownVan.Font = Enum.Font.GothamBlack
+ClownVan.Text = "ClownVan "
+ClownVan.TextColor3 = Color3.new(0, 0, 0)
+ClownVan.TextScaled = true
+ClownVan.TextSize = 14
+ClownVan.TextWrapped = true
+ 
+ 
+BoogaKuy.Name = "BoogaKuy"
+BoogaKuy.Parent = Frame
+BoogaKuy.BackgroundColor3 = Color3.new(1, 1, 0)
+BoogaKuy.Position = UDim2.new(0.00518736336, 0, 0.823245645, 0)
+BoogaKuy.Size = UDim2.new(0, 189, 0, 29)
+BoogaKuy.Font = Enum.Font.GothamBlack
+BoogaKuy.Text = "Mojo Craft"
+BoogaKuy.TextColor3 = Color3.new(0, 0, 0)
+BoogaKuy.TextScaled = true
+BoogaKuy.TextSize = 14
+BoogaKuy.TextWrapped = true
+ 
+ 
+Tec.Name = "Tec"
+Tec.Parent = Frame
+Tec.BackgroundColor3 = Color3.new(1, 0, 0)
+Tec.BorderColor3 = Color3.new(0.333333, 1, 1)
+Tec.Position = UDim2.new(0.487884015, 0, 0.0730539411, 0)
+Tec.Size = UDim2.new(0, 104, 0, 29)
+Tec.Font = Enum.Font.Gotham
+Tec.Text = "TEC-99"
+Tec.TextColor3 = Color3.new(0, 0, 0)
+Tec.TextScaled = true
+Tec.TextSize = 14
+Tec.TextWrapped = true
+ 
+ 
+COMINGSOON.Name = "COMING SOON"
+COMINGSOON.Parent = Frame
+COMINGSOON.BackgroundColor3 = Color3.new(0, 1, 0)
+COMINGSOON.Position = UDim2.new(0.291762769, 0, 0.822868645, 0)
+COMINGSOON.Size = UDim2.new(0, 95, 0, 31)
+COMINGSOON.Font = Enum.Font.GothamBlack
+COMINGSOON.Text = "COMING SOON"
+COMINGSOON.TextColor3 = Color3.new(0, 0, 0)
+COMINGSOON.TextScaled = true
+COMINGSOON.TextSize = 14
+COMINGSOON.TextWrapped = true
+ 
+ 
+guitest.Name = "guitest"
+guitest.Parent = Frame
+guitest.BackgroundColor3 = Color3.new(1, 0, 0.498039)
+guitest.Position = UDim2.new(0.292280287, 0, 0.741761625, 0)
+guitest.Size = UDim2.new(0, 200, 0, 28)
+guitest.Font = Enum.Font.GothamBlack
+guitest.Text = "GUI V0.2"
+guitest.TextColor3 = Color3.new(0, 0, 0)
+guitest.TextScaled = true
+guitest.TextSize = 14
+guitest.TextWrapped = true
+ 
+ 
+ComingSoon.Name = "Coming Soon"
+ComingSoon.Parent = Frame
+ComingSoon.BackgroundColor3 = Color3.new(0.333333, 0.666667, 1)
+ComingSoon.Position = UDim2.new(0.436579823, 0, 0.822076797, 0)
+ComingSoon.Size = UDim2.new(0, 96, 0, 31)
+ComingSoon.Font = Enum.Font.GothamBlack
+ComingSoon.Text = "COMING SOON"
+ComingSoon.TextColor3 = Color3.new(0, 0, 0)
+ComingSoon.TextScaled = true
+ComingSoon.TextSize = 14
+ComingSoon.TextWrapped = true
+ 
+ 
+powerbyguest.Name = "powerbyguest"
+powerbyguest.Parent = Frame
+powerbyguest.BackgroundColor3 = Color3.new(0.333333, 1, 1)
+powerbyguest.Position = UDim2.new(0.280850977, 0, 0.912348568, 0)
+powerbyguest.Size = UDim2.new(0, 296, 0, 29)
+powerbyguest.Font = Enum.Font.GothamBlack
+powerbyguest.Text = "POWER BY TEAM GUESTNOOB"
+powerbyguest.TextColor3 = Color3.new(0, 0, 0)
+powerbyguest.TextScaled = true
+powerbyguest.TextSize = 14
+powerbyguest.TextWrapped = true
+ 
+ 
+BootyOffenderV2.Name = "BootyOffenderV2"
+BootyOffenderV2.Parent = Frame
+BootyOffenderV2.BackgroundColor3 = Color3.new(1, 0, 0)
+BootyOffenderV2.BorderColor3 = Color3.new(0, 1, 1)
+BootyOffenderV2.Position = UDim2.new(0.0216164365, 0, 0.289526224, 0)
+BootyOffenderV2.Size = UDim2.new(0, 105, 0, 38)
+BootyOffenderV2.Font = Enum.Font.GothamBlack
+BootyOffenderV2.Text = "BootyOffenderV2 "
+BootyOffenderV2.TextColor3 = Color3.new(0, 0, 0)
+BootyOffenderV2.TextScaled = true
+BootyOffenderV2.TextSize = 14
+BootyOffenderV2.TextWrapped = true
+ 
+ 
+AnimationHUB.Name = "Animation HUB"
+AnimationHUB.Parent = Frame
+AnimationHUB.BackgroundColor3 = Color3.new(1, 0, 0)
+AnimationHUB.BorderColor3 = Color3.new(0, 1, 1)
+AnimationHUB.Position = UDim2.new(0.174636155, 0, 0.289492011, 0)
+AnimationHUB.Size = UDim2.new(0, 104, 0, 39)
+AnimationHUB.Font = Enum.Font.GothamBlack
+AnimationHUB.Text = "AnimationHub"
+AnimationHUB.TextColor3 = Color3.new(0, 0, 0)
+AnimationHUB.TextScaled = true
+AnimationHUB.TextSize = 14
+AnimationHUB.TextWrapped = true
+ 
+ 
+SETTINGS.Name = "SETTINGS"
+SETTINGS.Parent = Frame
+SETTINGS.BackgroundColor3 = Color3.new(1, 0, 0)
+SETTINGS.BorderColor3 = Color3.new(0, 1, 1)
+SETTINGS.Position = UDim2.new(0.800102293, 0, -0.0184594542, 0)
+SETTINGS.Size = UDim2.new(0, 82, 0, 24)
+SETTINGS.Font = Enum.Font.GothamBlack
+SETTINGS.Text = "SETTING"
+SETTINGS.TextColor3 = Color3.new(0, 0, 0)
+SETTINGS.TextScaled = true
+SETTINGS.TextSize = 14
+SETTINGS.TextWrapped = true
+ 
+ 
+Open.Name = "Open"
+Open.Parent = guitrollv10
+Open.BackgroundColor3 = Color3.new(1, 0, 0)
+Open.BorderColor3 = Color3.new(0, 1, 1)
+Open.Position = UDim2.new(-0.000508397818, 0, 0.760383964, 0)
+Open.Size = UDim2.new(0, 110, 0, 36)
+Open.Font = Enum.Font.SciFi
+Open.Text = "OPEN"
+Open.TextColor3 = Color3.new(0, 1, 1)
+Open.TextScaled = true
+Open.TextSize = 14
+Open.TextWrapped = true
+Open.MouseButton1Down:connect(function()
+Frame.Visible = true
+MAINFRAM.Visible = True
+Open.Visible = false
+end)
+ 
+-- Scripts:
+GrabKnifeV3.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/S8cjdEcs", true))()
+end)
+ 
+Wing.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/VndBubpZ", true))()
+end)
+ 
+Tec.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/94WRd1Uh", true))()
+end)
+ 
+MILK.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/qKhURcNk", true))()
+end)
+ 
+Gun.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/SeRxrgci", true))()
+end)
+ 
+KohlsAdmin.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/BDZxeY7S", true))()
+end)
+ 
+Shotgun.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/g7t4HRju", true))()
+end)
+ 
+BrutalAntiFurry.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/7jJx4jUe", true))()
+end)
+ 
+LGYHUB.MouseButton1Down:connect(function()
+loadstring(game:GetObjects("rbxassetid://1683559539")[1].Source)()
+end)
+ 
+ControlGui.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/fJbWYetZ", true))()
+end)
+ 
+AnimationHUB.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/31bdXgaZ", true))()
+end)
+ 
+BootyOffenderV2.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/nFxSYkM1", true))()
+end)
+ 
+BoogaKuy.MouseButton1Down:connect(function()
+loadstring(game:HttpGet(('https://pastebin.com/raw/NPc9MFnL'),true))()
+end)
+ 
+LuaHammer.MouseButton1Down:connect(function()
+loadstring(game:HttpGet("https://pastebin.com/raw/RKY5fTXh", true))()
+end)
